4993a3c90a3533cc1a2496959e3b8a7f3d6a1f5b
MatRabPC <matthew.rabski@uoit.net>
2017-03-13 15:35:33 -0400

    credits that we totally always do

--- hostd.c
+++ hostd.c
@@ -1,7 +1,7 @@
 /*
  * Host Dispatcher Shell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2017, <Samina Rahmanyar, Matthew Rabski>
  * All rights reserved.
  * 
  */
