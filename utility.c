38ab2b2a4a80abd3938295f3ada947acd2010081
githubsami <samina.rahmanyar@uoit.net>
2019-02-12 21:16:38 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -1,7 +1,7 @@
 /*
  * Host Dispatcher Shell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2017, <Samina Rahmanyar, Matthew Rabski>
+ * Copyright (C) 2015, <GROUP MEMBERS>
  * All rights reserved.
  * 
  */
